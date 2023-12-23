cd48b97740c3b203301fc8fd75a2ebb879a514ec
mohametalmeari <mohamet.almeari@gmail.com>
2023-12-23 20:17:51 +0300

    Style home page

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -2,6 +2,9 @@ import PropTypes from 'prop-types';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { archivePhoto, deletePhoto, likePhoto } from '../redux/posts/photoSlice';
+import {
+  ArchiveIcon, ArchivedIcon, DeleteIcon, EmptyStarIcon, FilledStarIcon,
+} from '../assets/icons';
 
 const PhotoCard = ({
   id, name, imageUrl, caption, likes, liked, control, navigator, archive,
@@ -23,37 +26,56 @@ const PhotoCard = ({
     dispatch(likePhoto({ id, liked }));
   };
   return (
-    <div>
+    <div className="photo-card">
       <p>
         {name}
       </p>
       {navigator
         ? (
           <NavLink to={`photos/${id}`}>
-            <img src={imageUrl} alt="i-share" />
+            <img className="photo-img" src={imageUrl} alt="i-share" />
           </NavLink>
         )
-        : <img src={imageUrl} alt="i-share" />}
+        : (
+          <div>
+            <img className="photo-img" src={imageUrl} alt="i-share" />
+          </div>
+        )}
       <p>
         {caption}
       </p>
-      <span>
-        {likes}
-      </span>
-      <button type="button" onClick={handleLike}>
-        {liked ? 'Unlike' : 'Like'}
-      </button>
-      {control && (
-      <>
-        <button type="button" onClick={handleDelete}>
-          Delete
-        </button>
-        <button type="button" onClick={handleArchive}>
-          {archive ? 'Unarchive' : 'Archive'}
-        </button>
-      </>
 
-      )}
+      <div className="control-container">
+        <button className="icon-btn" type="button" onClick={handleLike}>
+          {liked
+            ? (<FilledStarIcon /> || 'Unlike')
+            : (<EmptyStarIcon /> || 'Like')}
+        </button>
+        {control && (
+        <>
+          <button className="icon-btn" type="button" onClick={handleDelete}>
+            {<DeleteIcon /> || 'delete'}
+          </button>
+          <button className="icon-btn" type="button" onClick={handleArchive}>
+            {archive
+              ? (<ArchivedIcon /> || 'Unarchive')
+              : (<ArchiveIcon /> || 'Archive')}
+          </button>
+        </>
+        )}
+      </div>
+
+      <span className="interaction-counts">
+        {likes === 0
+          ? 'No stars'
+          : ''}
+        {likes > 1
+          ? `${likes} stars`
+          : ''}
+        {likes === 1
+          ? 'One star'
+          : ''}
+      </span>
     </div>
   );
 };
